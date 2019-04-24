9732b0d66f0465cbe0a527ae92038fea3f324516
Jason Patterson <Jason.Patterson@dowjones.com>
2019-04-24 16:19:25 -0400

    Add KiwiCore target

---
+++ KiwiCore/KiwiCore.h
@@ -0,0 +1,18 @@
+//
+//  KiwiCore.h
+//  KiwiCore
+//
+//  Created by Jason Patterson on 4/24/19.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KiwiCore.
+FOUNDATION_EXPORT double KiwiCoreVersionNumber;
+
+//! Project version string for KiwiCore.
+FOUNDATION_EXPORT const unsigned char KiwiCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KiwiCore/PublicHeader.h>
+
+
